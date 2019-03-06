ee56c83880e5526dbbf94d8cd90dd646b70bc4ba
Allison Thackston <allison@lyonthackston.com>
2019-03-05 23:10:16 -0800

    clean up entrypoint.sh

--- examples/ros2_crystal_example_dev/entrypoint.sh
+++ examples/ros2_crystal_example_dev/entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
 source /opt/ros/crystal/setup.bash
-
-# function build {
-#     su - $USER && colcon build --base-paths $1 $@
-# }
-
-# env
-
-# echo "building.." && su - $USER && colcon build --base-paths $1 $@
-
 exec "$@"
