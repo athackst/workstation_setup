9b059647f0171b16a9e6c841a02f7943038fda33
Allison Thackston <allison@lyonthackston.com>
2019-03-29 17:07:42 -0700

    added arch to google chrome source

--- scripts/install_base.sh
+++ scripts/install_base.sh
@@ -17,7 +17,7 @@ sudo apt-get install -y \
 
 # get chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
 sudo apt-get install -y google-chrome-stable
 
