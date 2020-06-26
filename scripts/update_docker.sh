bc23a72d16b24e592beddbaa6beb916025bb50b9
Allison Thackston <allison@lyonthackston.com>
2020-06-25 23:49:37 -0700

    added update_docker script

---
+++ scripts/update_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+docker pull ubuntu:20.04
+docker pull ubuntu:18.04
+# kinetic images needed for deepracer 
+docker pull athackst/ros:kinetic-base
+docker pull athackst/ros:kinetic-dev
+docker pull athackst/ros:melodic-base
+docker pull athackst/ros:melodic-dev
+docker pull athackst/ros:noetic-base
+docker pull athackst/ros:noetic-dev
+docker pull athackst/ros2:eloquent-base
+docker pull athackst/ros2:eloquent-dev
+docker pull athackst/ros2:foxy-base
+docker pull athackst/ros2:foxy-dev
+docker pull athackst/mkdocs-simple-plugin
+docker pull athackst/gh-pages-dev
+
+docker system prune -f
