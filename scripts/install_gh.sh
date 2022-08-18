03ff25a86dabdfb7997780a704a444e439a91b8a
Allison Thackston <allison@allisonthackston.com>
2022-08-18 05:22:53 -0700

    Add gh completion to install script

--- scripts/install_gh.sh
+++ scripts/install_gh.sh
@@ -3,4 +3,5 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 sudo apt-add-repository https://cli.github.com/packages
 sudo apt-get update
-sudo apt-get install -y gh
+sudo apt-get install -y gh bash-completion
+eval "$(gh completion -s bash)"
