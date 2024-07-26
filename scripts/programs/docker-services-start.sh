548aa3c92ad715e6fb0acd72f92848d077b34abe
Allison Thackston <allison@allisonthackston.com>
2024-07-26 06:58:06 +0000

    Update program docs

--- scripts/programs/docker-services-start.sh
+++ scripts/programs/docker-services-start.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# md
+# ## docker-services-start
+#
+# Starts all my common docker services
+#
+# ### Usage
+#
+# ```sh
+# docker-services-start
+# ```
+#
+# ### Dockers
+#
+# - registry
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
+# - notes mkdocs server
 docker run -d -p 8123:8000 --restart=always --name notes -v ~/Notes:/docs -e THEME=material -e SITE_DIR="/test" althack/mkdocs-simple-plugin:latest
+# - watchtower
 docker run -d --restart=always --name watchtower --volume /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --include-restarting --cleanup
--- scripts/programs/user-config-diff.sh
+++ scripts/programs/user-config-diff.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+
+# md
+# ## docker-services-stop
+#
 # This script compares the files in the home directory with those in workstation_setup/user
-# by opening up diffmerge so that changes can be captured in this repo.
+# by opening up diffmerge so that changes can be captured in this repo.[^1]
+#
+# ### Usage
+#
+# ```sh
+# user-config-diff
+# ```
+#
+# [^1]: Location of this project must be in ~/Code
+# /md
 
 path="$HOME/Code/workstation_setup/user"
 
