8b681c2785f514e30e6c02b1799e7bb7660fe042
Allison Thackston <6098197+athackst@users.noreply.github.com>
2026-07-25 13:55:17 -0700

    Use distro Bundler for Jekyll setup

--- setup/jekyll.sh
+++ setup/jekyll.sh
@@ -9,14 +9,6 @@ sudo apt-get update
 # install packages for jekyll
 sudo apt-get install -y \
     ruby \
+    ruby-bundler \
     ruby-dev \
     rbenv
-
-# install bundler
-gem install bundler --user
-
-# install pre-reqs for github-pages
-sudo apt-get install -y \
-    build-essential \
-    libxml2 \
-    libxml2-dev
