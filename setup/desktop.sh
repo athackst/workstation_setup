dca7038484bf1dc9ed3e0710489e0fa052c7d63e
Allison Thackston <6098197+athackst@users.noreply.github.com>
2026-07-26 09:45:59 -0700

    Load Tilingshell settings during desktop setup (#205)

--- setup/desktop.sh
+++ setup/desktop.sh
@@ -1,5 +1,7 @@
-#/bin/sh
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
 # Install tiling shell gnome extension
 sudo apt-get install -y gnome-shell-extension-manager
@@ -7,3 +9,6 @@ pip install -U gnome-extensions-cli
 
 gext install tilingshell@ferrarodomenico.com
 gext enable tilingshell@ferrarodomenico.com
+
+dconf load /org/gnome/shell/extensions/tilingshell/ \
+    < "$SCRIPT_DIR/tilingshell-settings-dump.txt"
